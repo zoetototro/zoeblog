6e920a7dd88e5abc684202a8372abed91da01729
zoetototro <enoshima.tototro2@gmail.com>
2020-12-31 15:48:12 +0900

    logo変更

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,11 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      ぞえのtwitter{' '}
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
       </div>
     </footer>
   </div>
