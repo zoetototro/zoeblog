a04c5761b9a907df679a4ebd21c08f802c27376c
zoetototro <enoshima.tototro2@gmail.com>
2021-01-04 16:24:04 +0900

    socialリンクの追加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,16 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      ぞえのtwitter{' '}
+      ぞえのTwitter{' '}
+      <a href="https://twitter.com/AKakizoe">@AKakizoe</a>
+    </h2>
+    <h2 className="taCenter">
+      Youtube{' '}
+      <a href="https://www.youtube.com/channel/UCoyfLsjhmKVWsjOyEAJI-nw?pbjreload=102">ぞえのエンジニア日記</a>
+    </h2>
+    <h2 className="taCenter">
+      Line@{' '}
+      <a href="https://lin.ee/3l2HJ9J">ぞえのライン@</a>
     </h2>
     <br />
     <footer className="footer">
